79f36628dd4c73d3b44762ed63ef80d00f2f3ff5
Evgeny Asmharin <evgeniiashmarin@gmail.com>
2024-03-03 15:24:05 +0700

    fixed invalid order of arguments

--- Worker/src/main/java/ru/nsu/fit/port/Controller.java
+++ Worker/src/main/java/ru/nsu/fit/port/Controller.java
@@ -17,6 +17,6 @@ public class Controller {
 
     @PostMapping(WorkerUrl.WORKER_CRACK_REQUEST)
     public void processManagerTask(@RequestBody WorkerCrackRequest request) {
-        worker.processManagerTask(request.requestId(), request.hash(), request.maxLength(), request.partCount(), request.partNumber());
+        worker.processManagerTask(request.requestId(), request.hash(), request.maxLength(), request.partNumber(), request.partCount());
     }
 }
